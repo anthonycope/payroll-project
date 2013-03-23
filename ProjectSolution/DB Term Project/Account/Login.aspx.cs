f5c3ea3ba2f523004ffa1b8cdf4915ebb47a0192
anthonycope <anthonycope@mail.usf.edu>
2013-03-22 23:53:23 -0400

    Added the project files. Note the actual db file isn't included. Manually add the .mdf to the Database Files folder on your own machine.

---
+++ ProjectSolution/DB Term Project/Account/Login.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DB_Term_Project.Account
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        string adminPassword = "f%x`gAC3[y{6L1z0";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           //RegisterHyperLink.NavigateUrl = "Register.aspx?ReturnUrl=" + HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+        }
+
+        protected void LoginUser_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Allows login as administrator to create an account
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (LoginUser.UserName == "admin" && LoginUser.Password == adminPassword)
+                Response.Redirect("/Account/Register.aspx");
+        }
+    }
+}
---
+++ ProjectSolution/DB Term Project/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="DB_Term_Project._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        HOME page under construction
+    </h2>
+    <p>
+        &nbsp;</p>
+</asp:Content>
