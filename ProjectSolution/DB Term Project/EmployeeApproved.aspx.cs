000c4d63dcdbc11d7fe8b877b873b35334c594ff
Jesse Brizzi <jesseb@mail.usf.edu>
2013-04-22 00:17:30 -0400

    Added page to view employees approval history

---
+++ ProjectSolution/DB Term Project/EmployeeApproved.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DB_Term_Project
+{
+    public partial class EmployeeApproved : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataSource1.SelectCommand = "SELECT [weekOf], [mgrid], [Amount], [Hours] FROM [Approval] WHERE eid = " + DropDownList1.SelectedValue + " AND Approved = 'Approved'";
+            SqlDataSource2.SelectCommand = "SELECT [weekOf], [Amount], [Hours], [mgrid] FROM [Approval] WHERE eid = " + DropDownList1.SelectedValue + " AND APPROVED = 'Awaiting Approval'";
+            GridView1.DataBind();
+            GridView2.DataBind();
+        }
+    }
+}
