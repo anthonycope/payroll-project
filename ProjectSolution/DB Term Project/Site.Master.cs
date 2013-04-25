d78ebbfff86ec6c6cf18f09af4448996de798f31
Henry Williams <henryw@mail.usf.edu>
2013-04-25 10:12:29 -0400

    Fixed minor issue.

--- ProjectSolution/DB Term Project/Site.Master.cs
+++ ProjectSolution/DB Term Project/Site.Master.cs
@@ -105,7 +105,7 @@ namespace DB_Term_Project
            }
            else //Redirect user to add hours page upon login
            {
-              Response.Redirect("~/Account/Login.aspx");
+              Response.Redirect("~/AddHours.aspx");
            }
         }
     }
